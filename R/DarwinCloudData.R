f8ea58cb5092d9c32bd19849c1309e257ddfbaf8
PoGibas <povilasgibas@gmail.com>
2018-08-14 20:29:55 +0300

    Removed getCloudData as users won't need it and exported downloadCloudData

--- R/DarwinCloudData.R
+++ R/DarwinCloudData.R
@@ -1,25 +1,3 @@
-#' Retrieve Darwin Cloud Data
-#' 
-#' `getCloudData()` is a function (not exported) to download and save
-#' Darwin Cloud data. This data is used to rename user data with function
-#' `darwinazeNames()`. Additionally this function saves date of the download so
-#' that this information could be shown in a `shiny` app. Therefore we create 
-#' a list (Darwind Cloud data and time stamp (`Sys.Date()`)).
-#' 
-#' @param pathCloud Path to Darwin Cloud data.
-#' 
-#' @return NULL.
-#' 
-#' @examples \dontrun{
-#'     getCloudData()
-#' }
-#' 
-getCloudData <- function(pathCloud = "https://raw.githubusercontent.com/kurator-org/kurator-validation/master/packages/kurator_dwca/data/vocabularies/darwin_cloud.txt") {
-    dataDarwinCloud <- list(data = downloadCloudData(pathCloud), date = Sys.Date())
-    saveRDS(dataDarwinCloud, "dataDarwinCloud.RDS")
-    return(NULL)
-}
-
 #' Download Darwin Cloud Data
 #' 
 #' `downloadCloudData()` is a function (not exported) to download Darwin Cloud 
@@ -31,6 +9,8 @@ getCloudData <- function(pathCloud = "https://raw.githubusercontent.com/kurator-
 #' @param columnStandard Name of the column that contains standard information.
 #' 
 #' @return data.frame of Darwin Cloud data.
+#' 
+#' @export
 #' 
 downloadCloudData <- function(pathCloud = NULL, columnField = "fieldname", columnStandard = "standard") {
     # Using data.table::fread for speed
