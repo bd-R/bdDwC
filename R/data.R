016a940417590f6c7b40684911a53e0823e9b62b
PoGibas <povilasgibas@gmail.com>
2018-08-20 12:06:14 +0300

    V 0.1.6 Added example data to the package (Indian reptiles)

--- R/data.R
+++ R/data.R
@@ -1,11 +1,20 @@
-#' Retrieve Darwin Cloud Data
+#' Darwin Cloud Data
 #' 
-#' Darwin Cloud data used to Darwinize
+#' Darwin Cloud data used to Darwinize. Darwin Cloud data was retrieved using 
+#' `bdDwC::downloadCloudData()` function. This dataset contains Darwin Cloud data
+#' and download date.
 #' 
-#' Data was retrieved using `bdDwC:::getCloudData()` function and contains
-#' Darwin Cloud data and download date.
+#' @format A list of length two. First entry contains a data frame with 
+#' 422 rows and 2 columns. Second entry contains a `Date` class object.
 #' 
-#' @name dataDarwinCloud
-#' @docType data
-#'
-NULL
+"dataDarwinCloud"
+
+
+#' Indian Reptiles
+#' 
+#' Dataset that contains subsample of Indian reptile observations 
+#' from the iNaturalist platform.
+#' 
+#' @format A data frame with 50 rows and 41 columns.
+#' 
+"dataReptiles"
